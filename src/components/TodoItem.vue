7d9e8f67bc24eeb8da21e77b2f219e1f1950c213
aliali2003 <aliali2003@MacBook-Air-aliali2003.local>
2021-08-19 23:36:36 +0300

    First

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="card">
+  <div class="forBtn">
+      <button class="btn trash" v-on:click="$emit('del', product.id)">
+        <img class="trash-img" src="../assets/del.svg" >
+      </button>
+    </div>
+    <img class="card__image" v-bind:src="product.imgUrl" width="332px" height="200px">
+    <h2>{{product.name}}</h2>
+    <h4>{{product.description}}</h4>
+    <h4>{{product.price}}$</h4>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TodoItem',
+  props: {
+    product: {},
+    methods: {}
+  }
+}
+</script>
+
+<style>
+.btn {
+  width: 50px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  z-index: 14;
+  right: -20px;
+  top: -20px;
+  display: none;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent !important;
+}
+
+.card {
+  margin-bottom: 40px;
+  margin-left: 40px;
+  width: 332px;
+  height: 423px;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
+  border-radius: 4px;
+  box-sizing: border-box;
+  position: relative;
+}
+.card__image {
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+}
+.card:hover .btn {
+  display: flex;
+}
+
+.forBtn {
+  display: flex;
+  justify-content: flex-end;
+}
+
+h2 {
+  text-align: center;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="father">
+<!--    <ul>-->
+<!--      <li v-for="(todo, a) of todos">{{todo}}: {{a + 1}}</li>-->
+<!--    </ul>-->
+<!--    <TodoItem name="Nikita"/>-->
+    <div id="productRegistration">
+      <h4>{{productName}}</h4>
+      <input
+          id="Input"
+          type="text"
+          v-bind:placeholder="namePlaceHolder"
+          v-model="nameValue"
+      />
+      <h4>{{productDescription}}</h4>
+      <textarea
+          v-bind:placeholder="dexscriptionPlaceHolder"
+          v-model="descriptionValue"
+          class="description"
+      ></textarea>
+      <h4>{{imgSrc}}</h4>
+      <input
+          id="Input"
+          type="text"
+          v-bind:placeholder="imgSrcPlaceHolder"
+          v-model="imgSrcValue"
+      />
+      <h4>{{productPrice}}</h4>
+      <input
+          id="Input"
+          type="text"
+          v-bind:placeholder="productPricwPlaceHolder"
+          v-model="priceValue"
+      />
+      <button id="btn" v-on:click="notesAdd">{{buttonPlaceHolder}}</button>
+    </div>
+    <div class="products">
+      <!--        <p>Cards</p>-->
+      <TodoItem
+          v-for="(product) in products"
+          :key="product.id"
+          :product="product"
+          @del="del"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+export default {
+  name: 'TodoList',
+  components: {TodoItem},
+  data() {
+    return {
+      nameValue: '',
+      imgSrcValue: '',
+      descriptionValue: '',
+      priceValue: '',
+
+      products: [{
+        id: 0,
+        name: 'Car',
+        description: 'Big Red Car Whit Wheals And With Machine',
+        imgUrl: 'http://wpsites.net/wp-content/uploads/2013/09/logo-2.png',
+        price: '100'
+      }],
+
+      productName: 'Наименование товара',
+      productDescription: 'Описание Товара',
+      dexscriptionPlaceHolder: 'Введите описнание товара',
+      namePlaceHolder: 'Введите наименование товара',
+      imgSrc: 'Ссылка на изображения товара',
+      imgSrcPlaceHolder: 'Введите ссылку',
+      productPrice: 'Цена товара',
+      productPricwPlaceHolder: 'Введите цену',
+      buttonPlaceHolder: 'Добавить Товар'
+    }
+  },
+  methods: {
+    notesAdd() {
+      if (this.priceValue === '') {
+        alert('Есть пустое поле')
+      } else if (this.nameValue === '') {
+        alert('Есть пустое поле')
+      } else if (this.descriptionValue === '') {
+        alert('Есть пустое поле')
+      } else {
+        this.products.push({
+          id: this.products.length,
+          name: this.nameValue,
+          description: this.descriptionValue,
+          imgUrl: this.imgSrcValue,
+          price: this.priceValue
+      })
+    }
+    },
+    del(id) {
+      this.products = this.products.filter(product => {
+        if (product.id !== id)
+          return product
+      })
+    }
+  }
+}
+</script>
+
+<style>
+  ul {
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .products {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  h4 {
+    margin-left: 30px;
+  }
+
+  #productRegistration {
+    padding-top: 5px;
+    width: 350px;
+    height: 600px;
+    box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.15);
+    box-sizing: border-box;
+  }
+
+  .father {
+    display: flex;
+    margin-left: 40px;
+  }
+
+  #Input {
+    width: 200px;
+    padding: 10px 10px;
+    border: none;
+    box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.15);
+    margin-left: 30px;
+    border-radius: 20px;
+    outline: none;
+  }
+
+  .description {
+    padding: 10px 10px;
+    width: 244px;
+    height: 108px;
+    border: none;
+    box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.15);
+    margin-left: 30px;
+    border-radius: 20px;
+  }
+
+  #btn {
+    background-color: lightgrey;
+    color: black;
+    margin-left: 30px;
+    margin-top: 30px;
+    width: 293px;
+    height: 30px;
+    box-shadow: 0px 5px 8px 0px rgba(0, 0, 0, 0.15);
+    border: none;
+    border-radius: 20px;
+  }
+
+  #btn:hover {
+    cursor: pointer;
+  }
+
+</style>
